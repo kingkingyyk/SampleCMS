99f0c6f338746526b951575c423d3bf7f3f44c80
YJG1 <janyang.yuan@3ds.com>
2018-09-13 17:26:46 +0800

    added dummy form

---
+++ src/ui/login/Dummy.java
@@ -0,0 +1,22 @@
+package ui.login;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+
+public class Dummy implements Initializable{
+    public static Dummy dummy=new Dummy();
+    @FXML private Label lblDisplay;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+    }
+}
